9eb8e53570ccd71d67ad6415855996f1ba12642c
Subhan <68732277+velaskuez@users.noreply.github.com>
2026-03-08 15:21:55 +0000

    int types

---
+++ include/int.h
@@ -0,0 +1,8 @@
+typedef signed char i8;
+typedef unsigned char u8;
+typedef signed short i16;
+typedef unsigned short u16;
+typedef unsigned u32;
+typedef signed i32;
+typedef unsigned long u64;
+typedef signed long i64;
