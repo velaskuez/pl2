f4c10ac9bd0235494d155641310ee635123bf42f
Subhan <68732277+velaskuez@users.noreply.github.com>
2026-03-09 13:48:09 +0000

    test token positions are correct

--- src/token.c
+++ src/token.c
@@ -21,7 +21,7 @@ static int isalphanumeric(char c) {
     return (c >= 'A' && c <= 'Z') || (c >= 'a' && c <= 'z') || (c >= '0' && c <= '9');
 }
 
-static void create_token(Tokeniser *self, TokenKind kind, String value);
+static void create_token(Tokeniser *self, TokenKind kind, String value, Position position);
 static char next_char(Tokeniser *self);
 static char peek_char(Tokeniser *self);
 static void skip_line(Tokeniser *self);
@@ -30,29 +30,31 @@ static TokenKind check_keyword(const String *word);
 
 int token_collect(Tokeniser *self) {
     for (;;) {
+        Position position = self->position;
+
         char c = next_char(self);
         if (!c) break;
         if (iswspace(c)) continue;
 
         switch (c) {
-        case ',': create_token(self, TokenComma, EMPTY_STRING); continue;
-        case '.': create_token(self, TokenDot, EMPTY_STRING); continue;
-        case '=': create_token(self, TokenEqual, EMPTY_STRING); continue;
-        case '>': create_token(self, TokenGt, EMPTY_STRING); continue;
-        case '{': create_token(self, TokenLCurly, EMPTY_STRING); continue;
-        case '[': create_token(self, TokenLBrack, EMPTY_STRING); continue;
-        case '(': create_token(self, TokenLParen, EMPTY_STRING); continue;
-        case '&': create_token(self, TokenAmpersand, EMPTY_STRING); continue;
-        case '|': create_token(self, TokenBar, EMPTY_STRING); continue;
-        case '<': create_token(self, TokenLt, EMPTY_STRING); continue;
-        case '-': create_token(self, TokenMinus, EMPTY_STRING); continue;
-        case '!': create_token(self, TokenExclamation, EMPTY_STRING); continue;
-        case '+': create_token(self, TokenPlus, EMPTY_STRING); continue;
-        case '}': create_token(self, TokenRCurly, EMPTY_STRING); continue;
-        case ']': create_token(self, TokenRBrack, EMPTY_STRING); continue;
-        case ')': create_token(self, TokenRParen, EMPTY_STRING); continue;
-        case ';': create_token(self, TokenSemicolon, EMPTY_STRING); continue;
-        case '*': create_token(self, TokenStar, EMPTY_STRING); continue;
+        case ',': create_token(self, TokenComma, EMPTY_STRING, position); continue;
+        case '.': create_token(self, TokenDot, EMPTY_STRING, position); continue;
+        case '=': create_token(self, TokenEqual, EMPTY_STRING, position); continue;
+        case '>': create_token(self, TokenGt, EMPTY_STRING, position); continue;
+        case '{': create_token(self, TokenLCurly, EMPTY_STRING, position); continue;
+        case '[': create_token(self, TokenLBrack, EMPTY_STRING, position); continue;
+        case '(': create_token(self, TokenLParen, EMPTY_STRING, position); continue;
+        case '&': create_token(self, TokenAmpersand, EMPTY_STRING, position); continue;
+        case '|': create_token(self, TokenBar, EMPTY_STRING, position); continue;
+        case '<': create_token(self, TokenLt, EMPTY_STRING, position); continue;
+        case '-': create_token(self, TokenMinus, EMPTY_STRING, position); continue;
+        case '!': create_token(self, TokenExclamation, EMPTY_STRING, position); continue;
+        case '+': create_token(self, TokenPlus, EMPTY_STRING, position); continue;
+        case '}': create_token(self, TokenRCurly, EMPTY_STRING, position); continue;
+        case ']': create_token(self, TokenRBrack, EMPTY_STRING, position); continue;
+        case ')': create_token(self, TokenRParen, EMPTY_STRING, position); continue;
+        case ';': create_token(self, TokenSemicolon, EMPTY_STRING, position); continue;
+        case '*': create_token(self, TokenStar, EMPTY_STRING, position); continue;
         case '/':
             // Skip comments
             c = peek_char(self);
@@ -61,14 +63,14 @@ int token_collect(Tokeniser *self) {
                 continue;
             }
 
-            create_token(self, TokenSlash, EMPTY_STRING);
+            create_token(self, TokenSlash, EMPTY_STRING, position);
             continue;
         case '0'...'9':
             String number = {0};
             append(&number, c);
             extend_while(self, &number, isnumber);
 
-            create_token(self, TokenNumber, number);
+            create_token(self, TokenNumber, number, position);
             continue;
         case '\'':
             String ch = {0};
@@ -85,7 +87,7 @@ int token_collect(Tokeniser *self) {
                 return -1;
             }
 
-            create_token(self, TokenChar, ch);
+            create_token(self, TokenChar, ch, position);
             continue;
 
         case '"':
@@ -97,7 +99,7 @@ int token_collect(Tokeniser *self) {
                 return -1;
             }
 
-            create_token(self, TokenString, str);
+            create_token(self, TokenString, str, position);
             continue;
         case 'A'...'Z':
         case 'a'...'z':
@@ -108,23 +110,23 @@ int token_collect(Tokeniser *self) {
             TokenKind kind = check_keyword(&word);
             if (!kind) kind = TokenIdent;
 
-            create_token(self, kind, word);
+            create_token(self, kind, word, position);
             continue;
         default:
             return -1;
         }
     }
 
-    create_token(self, TokenEof, EMPTY_STRING);
+    create_token(self, TokenEof, EMPTY_STRING, self->position);
 
     return 0;
 }
 
-void create_token(Tokeniser *self, TokenKind kind, String value) {
+void create_token(Tokeniser *self, TokenKind kind, String value, Position position) {
     Token token = {0};
     token.kind = kind;
     token.value = value;
-    token.position = self->position;
+    token.position = position;
     append(&self->tokens, token);
 }
 
--- tests/token_test.c
+++ tests/token_test.c
@@ -6,29 +6,25 @@
 
 int compare_tokens(const Tokens *have, const Token *want) {
     for (size_t i = 0; i < have->len; i++) {
+
         Token lhs = have->items[i];
         Token rhs = want[i];
 
-        if (lhs.kind != rhs.kind) {
-            return false;
-        }
+        if (lhs.kind != rhs.kind) return false;
 
-        if (lhs.value.items == nullptr && rhs.value.items == nullptr) {
-            return true;
-        }
+        if (lhs.value.items == nullptr && rhs.value.items == nullptr) return true;
 
         if ((lhs.value.items != nullptr && rhs.value.items == nullptr)
                 || (lhs.value.items == nullptr && rhs.value.items != nullptr)) {
-            return -1;
-        }
-
-        if (lhs.value.len != rhs.value.len) {
             return false;
         }
 
-        if (strcmp(lhs.value.items, rhs.value.items) != 0) {
-            return false;
-        }
+        if (lhs.value.len != rhs.value.len) return false;
+
+        if (strcmp(lhs.value.items, rhs.value.items) != 0) return false;
+
+        if (lhs.position.line != rhs.position.line) return false;
+        if (lhs.position.col != rhs.position.col) return false;
     }
 
     return true;
@@ -52,34 +48,59 @@ void test_collect(void) {
             "fn\n"
     );
 
-    // TODO: test positions
     Token want[] = {
-        { .kind = KeywordStruct },
-        { .kind = TokenComma },
-        { .kind = TokenDot },
-        { .kind = TokenEqual },
-        { .kind = TokenGt },
-        { .kind = TokenLCurly },
-        { .kind = TokenLBrack },
-        { .kind = TokenLParen },
-        { .kind = TokenAmpersand },
-        { .kind = TokenBar },
-        { .kind = TokenLt },
-        { .kind = TokenMinus },
-        { .kind = TokenExclamation },
-        { .kind = TokenPlus },
-        { .kind = TokenRCurly },
-        { .kind = TokenRBrack },
-        { .kind = TokenRParen },
-        { .kind = TokenSemicolon },
-        { .kind = TokenStar },
-        { .kind = TokenSlash },
-        { .kind = TokenNumber, .value = string_from_cstr("1234") },
-        { .kind = TokenIdent, .value = string_from_cstr("abcd") },
-        { .kind = TokenChar, .value = string_from_cstr("a") },
-        { .kind = TokenString, .value = string_from_cstr("Hello, World!") },
-        { .kind = KeywordFn },
-        { .kind = TokenEof },
+        { .kind = KeywordStruct, .value = string_from_cstr("struct"),
+          .position.col = 0, .position.line = 0 },
+        { .kind = TokenComma,
+          .position.col = 0, .position.line = 1},
+        { .kind = TokenDot,
+          .position.col = 1, .position.line = 1},
+        { .kind = TokenEqual,
+          .position.col = 2, .position.line = 1},
+        { .kind = TokenGt,
+          .position.col = 3, .position.line = 1},
+        { .kind = TokenLCurly,
+          .position.col = 4, .position.line = 1},
+        { .kind = TokenLBrack,
+          .position.col = 5, .position.line = 1},
+        { .kind = TokenLParen,
+          .position.col = 6, .position.line = 1},
+        { .kind = TokenAmpersand,
+          .position.col = 7, .position.line = 1},
+        { .kind = TokenBar,
+          .position.col = 8, .position.line = 1},
+        { .kind = TokenLt,
+          .position.col = 9, .position.line = 1},
+        { .kind = TokenMinus,
+          .position.col = 10, .position.line = 1},
+        { .kind = TokenExclamation,
+          .position.col = 11, .position.line = 1},
+        { .kind = TokenPlus,
+          .position.col = 12, .position.line = 1},
+        { .kind = TokenRCurly,
+          .position.col = 13, .position.line = 1},
+        { .kind = TokenRBrack,
+          .position.col = 14, .position.line = 1},
+        { .kind = TokenRParen,
+          .position.col = 15, .position.line = 1},
+        { .kind = TokenSemicolon,
+          .position.col = 16, .position.line = 1},
+        { .kind = TokenStar,
+          .position.col = 17, .position.line = 1},
+        { .kind = TokenSlash,
+          .position.col = 18, .position.line = 1},
+        { .kind = TokenNumber, .value = string_from_cstr("1234"),
+          .position.col = 0, .position.line = 3},
+        { .kind = TokenIdent, .value = string_from_cstr("abcd"),
+          .position.col = 0, .position.line = 4},
+        { .kind = TokenChar, .value = string_from_cstr("a"),
+          .position.col = 0, .position.line = 5},
+        { .kind = TokenString, .value = string_from_cstr("Hello, World!"),
+          .position.col = 0, .position.line = 6},
+        { .kind = KeywordFn, .value = string_from_cstr("fn"),
+          .position.col = 0, .position.line = 7},
+        { .kind = TokenEof,
+          .position.col = 0, .position.line = 8},
     };
 
     Tokeniser tokeniser = {0};
